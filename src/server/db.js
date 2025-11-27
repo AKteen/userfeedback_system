021a616211141131f3bdfe24fca80c9b539d4eb7
Aditya Kulkarni <vip.aditya.k@gmail.com>
2025-11-27 21:51:09 +0530

    Minor changes

--- src/server/db.js
+++ src/server/db.js
@@ -1,17 +1,15 @@
 import mongoose from 'mongoose';
 
-const connectDB = () => {
+const connectDB = async () => {
     try {
-        const conn = mongoose.connect("mongodb://localhost:27017/testing", {
+        const conn = await mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/testing", {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
-        console.log("MongoDB connected!");
-
-
+        console.log(`MongoDB connected: ${conn.connection.host}`);
     } catch (error) {
-        console.log("failed to connect mongoDB");
-
+        console.error(`MongoDB connection failed: ${error.message}`);
+        process.exit(1);
     }
 }
 
